0dc6ac0f2e0ceb3d40c206024dac001c0930128c
larisarapio <larissa.sarapio@gmail.com>
2025-09-01 22:07:13 -0300

    refactor: replace hardcoded core concepts with dynamic data import

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];  
-import reactImg from './assets/react-core-concepts.png';
+import { CORE_CONCEPTS } from './data.js';
 
 function getRondomInt(max) {
   return Math.floor(Math.random() * (max + 1));
@@ -20,17 +20,15 @@ function Header() {
   );
 }
 
-function CoreConcepts(props) {
+function CoreConcepts({title, description, image}) {
   return  (
     <li>
-      <img src={props.image} alt={props.title} />
-      <h3>{props.tile}</h3>
-      <p>{props.description}</p>
+      <img src={image} alt={title} />
+      <h3>{title}</h3>
+      <p>{description}</p>
     </li>
   )
-
 }
-
 
 function App() {
   return (
@@ -40,14 +38,14 @@ function App() {
         <section id="core-concepts">
         <h2>Core Concepts</h2>
         <ul>
-          <CoreConcepts 
-            title="Components"
-            description="The core UI building block of any React app"
-            image={reactImg}
-          />
-          <CoreConcepts title="Props" />
-          <CoreConcepts />
-          <CoreConcepts />
+          <CoreConcepts {...CORE_CONCEPTS[0]} />
+          <CoreConcepts
+            title={CORE_CONCEPTS[1].title}
+            description={CORE_CONCEPTS[1].description}
+            image={CORE_CONCEPTS[1].image}
+           />
+          <CoreConcepts {...CORE_CONCEPTS[2]} />
+          <CoreConcepts {...CORE_CONCEPTS[3]} />
         </ul>
         </section>
       </main>
